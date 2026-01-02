9c722cb1fba5826110c7bc19dc07563f853de08a
MFlisar <mflisar.development@gmail.com>
2026-01-02 23:01:04 +0100

    .

--- scripts/update-readme.ps1
+++ scripts/update-readme.ps1
@@ -1,3 +1,52 @@
+function Convert-MarkdownToHtml($markdown) {
+    $html = ConvertFrom-Markdown -InputObject $markdown
+    return $html.Html
+}
+
+function Get-Image($item, $defaultImage) {
+    return $item.PSObject.Properties['image'] ? $item.image : $defaultImage
+}
+
+function Build-Table($items, $maxImageWidth, $defaultImage) {
+    $table = "<table>`n"
+    $table += "  <tr><th>Image</th><th>Libary</th><th>Description</th></tr>`n"
+    foreach ($item in $items) {
+        $name = $item.name
+        $descHtml = Convert-MarkdownToHtml $item.description
+        $desc = $descHtml + "<br>" + "<img src='https://img.shields.io/maven-central/v/$($item.'main-maven-id')?label=&style=for-the-badge&labelColor=444444&color=grey' alt='maven version'/>"
+        $image = Get-Image $item $defaultImage
+        $repo_url = "https://github.com/MFlisar/$name"
+        $table += "  <tr>" +
+            "<td style='vertical-align: top;'><img src='$image' alt='Image' style='max-width:$maxImageWidth;'/></td>" +
+            "<td style='vertical-align: top;'><a href='$repo_url'>$name</a></td>" +
+            "<td style='vertical-align: top;'>$desc</td>" +
+            "</tr>`n"
+    }
+    $table += "</table>`n"
+    return $table
+}
+
+function Update-Readme($readmePath, $startMarker, $endMarker, $output) {
+    $readmeLines = Get-Content $readmePath
+    $startIdx = ($readmeLines | Select-String -Pattern ([regex]::Escape($startMarker))).LineNumber
+    $endIdx = ($readmeLines | Select-String -Pattern ([regex]::Escape($endMarker))).LineNumber
+    if ($startIdx -and $endIdx -and $endIdx -ge $startIdx) {
+        $startIdx = $startIdx - 1
+        $endIdx = $endIdx - 1
+        $before = $readmeLines[0..$startIdx]
+        $after = $readmeLines[$endIdx..($readmeLines.Count-1)]
+        $outputLines = $output -split "`n"
+        $newReadme = @($before) + $outputLines + $after
+        $newReadmeString = $newReadme -join "`n"
+        $newReadmeString = $newReadmeString -replace "(`n){3,}", "`n`n"
+        Set-Content -Path $readmePath -Value $newReadmeString
+    }
+}
+
+# --------------
+# - SKRIPT
+# --------------
+
 $JSON = "projects.json"
 $README = "README.md"
 $START_MARKER = "<!-- LIBRARIES START -->"
@@ -5,7 +54,6 @@ $END_MARKER = "<!-- LIBRARIES END -->"
 $DEFAULT_IMAGE = "media/kotlin-icon.png"
 $MAX_IMAGE_WIDTH = "200px"
 
-# Projekte-Liste aus JSON holen
 $jsonContent = Get-Content $JSON -Raw | ConvertFrom-Json
 $projects = $jsonContent[0].projects
 
@@ -14,48 +62,12 @@ foreach ($groupObj in $projects) {
     $group = $groupObj.group
     $open_flag = $groupObj.open
     $items = $groupObj.items
-    if ($open_flag -eq $true) {
-        $output += "`n<details open>`n`n"
-    } else {
-        $output += "`n<details>`n`n"
-    }
+    $output += "`n<details" + ($(if ($open_flag) { " open" } else { "" })) + ">`n`n"
     $output += "<summary>$group ($($items.Count))</summary><br>`n`n"
-    $output += "<table>`n"
-    $output += "  <tr><th>Image</th><th>Libary</th><th>Description</th></tr>`n"
-    foreach ($item in $items) {
-        $name = $item.name
-        $desc = $item.description + "<br>" + "<img src='https://img.shields.io/maven-central/v/$($item.'main-maven-id')?label=&style=for-the-badge&labelColor=444444&color=grey' alt='maven version'/>"
-        $image = if ($item.PSObject.Properties['image']) { $item.image } else { $DEFAULT_IMAGE }
-        $repo_url = "https://github.com/MFlisar/$name"
-        $output += "  <tr>" +
-            "<td valign='top'><img src='$image' alt='Image' style='max-width:$MAX_IMAGE_WIDTH;'/></td>" +
-            "<td valign='top'><a href='$repo_url'>$name</a></td>" +
-            "<td valign='top'>$desc</td>" +
-            "</tr>`n"
-    }
-    $output += "</table>`n"
+    $output += Build-Table $items $MAX_IMAGE_WIDTH $DEFAULT_IMAGE
     $output += "`n</details>`n`n"
 }
 
-
-# Bereich im README ersetzen (ohne Tempfile)
-$readmeLines = Get-Content $README
-
-# Index des Start- und End-Markers finden
-$startIdx = ($readmeLines | Select-String -Pattern ([regex]::Escape($START_MARKER))).LineNumber
-$endIdx = ($readmeLines | Select-String -Pattern ([regex]::Escape($END_MARKER))).LineNumber
-
-if ($startIdx -and $endIdx -and $endIdx -ge $startIdx) {
-    $startIdx = $startIdx - 1  # 0-basiert
-    $endIdx = $endIdx - 1
-    $before = $readmeLines[0..$startIdx]
-    $after = $readmeLines[$endIdx..($readmeLines.Count-1)]
-    $outputLines = $output -split "`n"
-    $newReadme = @($before) + $outputLines + $after
-    $newReadmeString = $newReadme -join "`n"
-    # Mehrfache Leerzeilen am Ende reduzieren
-    $newReadmeString = $newReadmeString -replace "(`n){3,}", "`n`n"
-    Set-Content -Path $README -Value $newReadmeString
-}
+Update-Readme $README $START_MARKER $END_MARKER $output
 
 Write-Host "README aktualisiert."
